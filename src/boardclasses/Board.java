d00bf17fca9c8e1b629e7eadb8e98d5ab825ddab
abokideveloper1 <xandykelvin@gmail.com>
2020-11-20 00:28:44 -0300

    Exceptions messages and defensive programming had been added

--- src/boardclasses/Board.java
+++ src/boardclasses/Board.java
@@ -9,6 +9,12 @@ public class Board {
 	private Pieces [][] pieces;
 	
 	public Board(int rows, int columns) {
+		
+		if(rows < 1 && columns < 1) {
+			
+			throw new BoardException("Error creating board,there must be at least 1 row and 1 column");
+		}
+		
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Pieces[rows][columns];
@@ -18,24 +24,29 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
 
 	public int getColumns() {
 		return columns;
 	}
 
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
-
   public Pieces pieces(int row,int column) {
+	  
+	  if(!positionExists(rows,columns)) {
+		  
+		  throw new BoardException("No position on board");
+		  
+	  }
 	  
 	  return pieces [row] [column];
   }
   
   public Pieces pieces(Positions positions) {
+	  
+	  if(!positionExists(rows,columns)) {
+		  
+		  throw new BoardException("No position on board");
+		  
+	  }
 	  
 	  return pieces [positions.getRows()] [positions.getColumns()];
 	  
@@ -43,10 +54,42 @@ public class Board {
   
   public void placePiece(Pieces piece,Positions position) {
 	  
+	  if(thereIsAPiece(position)) {
+		  
+		  throw new BoardException("There is already a piece on position " + position);
+		  
+	  }
+	  
 	  pieces[position.getRows()][position.getColumns()] = piece;
 	  piece.positions = position;
 	  
   }
+  
+  private boolean positionExists(int rows , int columns) {
+	  
+	  return rows >= 0 && rows <= rows && columns >=0 && columns<= columns;
+	  
+  }
+  
+  public boolean positionExists(Positions position) {
+	  
+	  return positionExists(position.getRows() , position.getColumns());
+	  
+  }
+  
+  public boolean thereIsAPiece(Positions position) {
+	  
+	  if(!positionExists(position)) {
+		  
+		  throw new BoardException("No position on board");
+		  
+	  }
+	  
+	 return pieces(position) != null;
+	  
+  }
+  
+  
 	
 	
 
---
+++ src/boardclasses/BoardException.java
@@ -0,0 +1,18 @@
+package boardclasses;
+
+public class BoardException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public BoardException(String msg) {
+		
+		super(msg);
+		
+	}
+	
+	
+
+}
