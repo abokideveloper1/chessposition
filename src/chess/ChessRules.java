76bd9d9d691e29a8b25276390e2025ca6b7bc4f6
abokideveloper1 <xandykelvin@gmail.com>
2020-11-21 02:58:02 -0300

    Board coordinates and exceptions for them had been added

--- src/chess/ChessRules.java
+++ src/chess/ChessRules.java
@@ -41,11 +41,20 @@ public class ChessRules {
 	return game;
 
 }
+	
+	private void placeNewPiece(char column,int row,ChessPiece piece) {
+		
+		
+		board.placePiece(piece, new ChessPosition(column,row).toPosition());
+		
+	}
+	
+	
 	private void initializeSetup() {
 		
-		board.placePiece(new Rook(board , Color.WHITE) , new Positions(2 , 1));
-		board.placePiece(new King(board , Color.BLACK),new Positions(0 ,4));
-		board.placePiece(new King(board , Color.WHITE), new Positions(7 ,4));
+		placeNewPiece('b', 6 ,new Rook(board , Color.WHITE));
+	placeNewPiece('e',8,new King(board , Color.BLACK));
+		placeNewPiece('e',1,new King(board , Color.WHITE));
 		
 	}
 }
